cf37b40166b40968e3beac79fb359c6391527233
jose laport <josepaulolaport@gmail.com>
2024-04-15 22:15:03 -0800

    test

--- tests/unit/test_routes.py
+++ tests/unit/test_routes.py
@@ -3,7 +3,3 @@ from project.main import app
 def test_home_route():
     response = app.test_client().get("/")
     assert response.status_code == 302
-
-def test_fake_route():
-    response = app.test_client().get("/fake")
-    assert response.status_code == 302
