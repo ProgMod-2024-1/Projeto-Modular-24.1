3bda55764453ce1e1fa17214f0f2a28df9459d15
jose laport <josepaulolaport@gmail.com>
2024-04-15 21:00:39 -0800

    test

--- tests/unit/test_routes.py
+++ tests/unit/test_routes.py
@@ -3,7 +3,3 @@ from project.main import app
 def test_home_route():
     response = app.test_client().get("/")
     assert response.status_code == 200
-
-def test_login_route():
-    response = app.test_client().get("/principal/login")
-    assert response.status_code == 200
