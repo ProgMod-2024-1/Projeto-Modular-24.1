338d055554689e0b32e364cf2d99b367bc6ed2af
josepaulolaport <102634412+josepaulolaport@users.noreply.github.com>
2024-04-15 21:14:49 -0800

    Update test_routes.py

--- tests/unit/test_routes.py
+++ tests/unit/test_routes.py
@@ -3,15 +3,3 @@ from project.main import app
 def test_home_route():
     response = app.test_client().get("/")
     assert response.status_code == 200
-
-def test_login_route():
-    response = app.test_client().get("/principal/login")
-    assert response.status_code == 200
-
-def test_register_route():
-    response = app.test_client().get("/principal/register")
-    assert response.status_code == 200
-
-def test_fake_route():
-    response = app.test_client().get("/principal/fake")
-    assert response.status_code == 200
