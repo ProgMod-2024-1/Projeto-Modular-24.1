8599c3c3e068aae3c34590f98d1169b53f4a49dc
jose laport <josepaulolaport@gmail.com>
2024-04-15 18:08:30 -0800

    feature: registrar usuario

--- project/blueprints/principal/principal.py
+++ project/blueprints/principal/principal.py
@@ -1,7 +1,24 @@
-from flask import Blueprint,render_template,redirect
+from flask import Blueprint,render_template,redirect, request, flash
+from project.blueprints.principal.principalService import register_user
 
-principal = Blueprint("principal",__name__,template_folder="templates")
+principal = Blueprint("principal",__name__,url_prefix= '/principal', template_folder="templates",static_folder="static")
 
 @principal.route("/")
-def index():
+def pagina_principal():
     return render_template("principal.html")
+
+@principal.route("/register", methods = ["POST", "GET"])
+def register():
+
+    if request.method =='POST':
+        result = register_user(username=request.form["username"], password=request.form["password"], permission=request.form["permission"])
+
+        if(result["success"] == 1):
+            pass
+
+        else:
+            flash(result["message"], "error")
+            return render_template("register.html", data = result["user"])
+    
+    else:
+        return render_template("register.html")
---
+++ project/blueprints/principal/principalRepo.py
@@ -0,0 +1,12 @@
+import os
+from typing import List
+from project.db.database import read_db, write_db
+
+USERS_DB_URI = os.path.join(os.path.dirname(os.path.abspath(__file__)),"database","users.json")
+
+def get_all_users()-> List[object]:
+    return read_db(USERS_DB_URI)
+
+def add_user(username: str, password: str, permission:int)->int:
+    print(USERS_DB_URI)
+    return write_db({"username": username, "password": password, "permission":permission},"username", USERS_DB_URI)
