d3e4adf257c716f5027e95f46ea0cb3c885700a9
jose laport <josepaulolaport@gmail.com>
2024-04-17 15:49:12 -0800

    fix: inicializacao

--- project/main.py
+++ project/main.py
@@ -5,10 +5,12 @@ from project.blueprints.principal.principalRepo import get_user
 from project.blueprints.principal.principalService import User
 
 app = Flask(__name__, static_folder="static", static_url_path="/static")
+app.register_blueprint(principal)
 app.secret_key = "senhaSecreta"
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'principal.user_login'
+
 @app.route("/")
 def access():
     return redirect(url_for('principal.pagina_principal'))
@@ -17,6 +19,7 @@ def access():
 def load_user(user_id):
     user_data = get_user(user_id)
     return User(user_data["username"], user_data["permission"])
-app.register_blueprint(principal)
-app.run()
+    
+if __name__ == '__main__':
+    app.run(debug=True)
 
