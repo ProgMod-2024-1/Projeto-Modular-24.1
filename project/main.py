7e890190c70272995911ab253f2443e4801a566b
josepaulolaport <102634412+josepaulolaport@users.noreply.github.com>
2024-04-17 15:58:21 -0800

    Feature/pagina principal e funcionalidade de login (#13)
    * feature: registrar usuario
    
    * test
    
    * test
    
    * Update python-app.yml
    
    * test
    
    * uncomment
    
    * test
    
    * message
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * fix: corrigindo chamadas ao banco em write e os arquivos json
    
    * feature: pagina principal e funcionalidade de login
    
    * fix: inicializacao

--- project/main.py
+++ project/main.py
@@ -1,12 +1,25 @@
 from project.blueprints.principal.principal import principal
 from flask import Flask, redirect, url_for
+from flask_login import LoginManager
+from project.blueprints.principal.principalRepo import get_user
+from project.blueprints.principal.principalService import User
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", static_url_path="/static")
 app.register_blueprint(principal)
+app.secret_key = "senhaSecreta"
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'principal.user_login'
 
 @app.route("/")
 def access():
     return redirect(url_for('principal.pagina_principal'))
+
+@login_manager.user_loader
+def load_user(user_id):
+    user_data = get_user(user_id)
+    return User(user_data["username"], user_data["permission"])
     
 if __name__ == '__main__':
     app.run(debug=True)
+
