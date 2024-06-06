3b834a98b23a137a54b301d2a74ff3f3633c2910
RafaelTolini <rftolini@gmail.com>
2024-06-05 21:38:06 -0300

    chore: comentários

--- project/blueprints/avaliacao/avaliacaoRepo.py
+++ project/blueprints/avaliacao/avaliacaoRepo.py
@@ -6,13 +6,16 @@ from project.db.database import read_db, write_db, update_db, delete_db
 
 AVALIACOES_DB_URI = os.path.join(os.path.dirname(os.path.abspath(__file__)),"database","avaliacao.json")
 
+#Retorna todas as avalçiações presentes no json
 def get_all_avaliacoes()-> List[object]:
     return read_db(AVALIACOES_DB_URI)
 
+#Registra uma nova avaliação
 def registra_avaliacao(turma: str, codAval: str, curso:str, perguntas: object)->int:
 
     return write_db([{"perguntas":perguntas, "instancias":[], "info": {"turma": turma, "codAval": codAval, "curso": curso, "corretor":""}}], "info", AVALIACOES_DB_URI)
 
+#Retorna uma avaliação específica com os dados fornecidos
 def seek_avaliacao(turma: str, codAval: str, curso:str)-> object:
     data = read_db(AVALIACOES_DB_URI)
 
@@ -22,6 +25,7 @@ def seek_avaliacao(turma: str, codAval: str, curso:str)-> object:
 
     return None
 
+#Atualiza os dados de uma avaliação existente
 def muda_avaliacao(turma: str, codAval: str, curso:str, perguntas: object, instancias: object, corretor: str)->int:
 
     return update_db({"perguntas":perguntas, "instancias":instancias, "info": {"turma": turma, "codAval": codAval, "curso": curso, "corretor":corretor}}, "info", AVALIACOES_DB_URI)
--- project/blueprints/avaliacao/avaliacaoService.py
+++ project/blueprints/avaliacao/avaliacaoService.py
@@ -1,9 +1,11 @@
 from project.blueprints.avaliacao.avaliacaoRepo import get_all_avaliacoes, registra_avaliacao, seek_avaliacao, muda_avaliacao
 
-
+#Retorna todas as avalçiações presentes no json
 def get_avaliacoes():
     return get_all_avaliacoes()
 
+
+#Registra uma nova avaliação
 def registra_avaliacoes(turma,codAval,curso,perguntas):
     result = registra_avaliacao(turma=turma, codAval=codAval,curso=curso, perguntas=perguntas)
     if result == 1:
@@ -24,11 +26,14 @@ def registra_avaliacoes(turma,codAval,curso,perguntas):
                 "message": "Ocorreu um erro ao criar a avaliação. Tente novamente mais tarde",
                 "user":{"curso":curso,"turma":turma}
                 }
-    
+
+
+#Retorna uma avaliação específica com os dados fornecidos   
 def seek_avaliacoes(turma, codAval, curso):
     return seek_avaliacao(turma, codAval, curso)
 
-    
+
+#Atualiza os dados de uma avaliação existente   
 def muda_avaliacoes(turma,codAval,curso,perguntas, instancias, corretor):
     result = muda_avaliacao(turma=turma, codAval=codAval,curso=curso, perguntas=perguntas, instancias=instancias, corretor=corretor)
     if result == 1:
