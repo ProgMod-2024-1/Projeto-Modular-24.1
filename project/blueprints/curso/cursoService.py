99102337408bde6d789761ac2d6587042b2f95a2
Gustavo Molina Soares <97996136+guzsoares@users.noreply.github.com>
2024-06-11 17:33:30 -0300

    Update cursoService.py

--- project/blueprints/curso/cursoService.py
+++ project/blueprints/curso/cursoService.py
@@ -1,17 +1,35 @@
 from cursoRepo import get_curso, update_curso
 
-def defPreReq(db_path, codCurs, codPreReq):
-    curso = get_curso(db_path, codCurs)
+def register_pre_requisito(codCurs: str, codPreReq: str) -> object:
+    curso = get_curso(codCurs)
     if curso:
-        if codPreReq not in curso['preRequisitos']:
-            curso['preRequisitos'].append(codPreReq)
-            return update_curso(db_path, curso)
-    return False
+        if codPreReq not in curso["preRequisitos"]:
+            curso["preRequisitos"].append(codPreReq)
+            if update_curso(curso):
+                return {
+                    "success": 1,
+                    "message": "Pré-requisito adicionado com sucesso",
+                    "curso": curso
+                }
+    return {
+        "success": 0,
+        "message": "Falha ao adicionar pré-requisito ou pré-requisito já existente",
+        "curso": None
+    }
 
-def defProfDispCurs(db_path, codCurs, matrProf):
-    curso = get_curso(db_path, codCurs)
+def register_professor_disponivel(codCurs: str, matrProf: str) -> object:
+    curso = get_curso(codCurs)
     if curso:
-        if matrProf not in curso['professoresDisponiveis']:
-            curso['professoresDisponiveis'].append(matrProf)
-            return update_curso(db_path, curso)
-    return False
+        if matrProf not in curso["professoresDisponiveis"]:
+            curso["professoresDisponiveis"].append(matrProf)
+            if update_curso(curso):
+                return {
+                    "success": 1,
+                    "message": "Professor disponível adicionado com sucesso",
+                    "curso": curso
+                }
+    return {
+        "success": 0,
+        "message": "Falha ao adicionar professor disponível ou professor já existente",
+        "curso": None
+    }
