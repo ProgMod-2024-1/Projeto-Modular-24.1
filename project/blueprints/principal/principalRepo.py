c642a79390c2e10ab935f629c0ec0c8f999eadfa
jose laport <josepaulolaport@gmail.com>
2024-06-19 19:11:58 -0800

    initial commit

--- project/blueprints/principal/principalRepo.py
+++ project/blueprints/principal/principalRepo.py
@@ -1,17 +1,16 @@
 import os
 from typing import List
 from project.db.database import read_db, write_db
-
-USERS_DB_URI = os.path.join(os.path.dirname(os.path.abspath(__file__)),"database","users.json")
+from flask import session
 
 def get_all_users()-> List[object]:
-    return read_db(USERS_DB_URI)
+    return read_db("users")
 
-def add_user(username: str, password: str, permission:int)->int:
-    return write_db([{"username": username, "password": password, "permission":permission}],"username", USERS_DB_URI)
+def create_user(username: str, password: str, permission:int)->int:
+    return write_db([{"username": username, "password": password, "permission":permission}],"username", "users")
 
 def get_user(username:str)-> object:
-    data = read_db(USERS_DB_URI)
+    data = read_db("users")
     for user in data:
         if(user["username"]==username):
             return user
